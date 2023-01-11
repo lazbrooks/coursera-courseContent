973cb57727485dead54255ccf9e5a1f9ac950d66
lazbrooks <ltrbrooks@gmail.com>
2023-01-11 23:37:50 +0000

    Add files via upload
    Uploading a file

---
+++ email.py
@@ -0,0 +1,20 @@
+#An Email Simulation
+
+ 
+
+
+
+user_choice = ""
+
+while user_choice != "quit":
+    user_choice = input("What would you like to do - read/mark spam/send/quit?")
+    if user_choice == "read":
+        pass #Place your logic here
+    elif user_choice == "mark spam":
+        pass #Place your logic here
+    elif user_choice == "send":
+        pass #Place your logic here
+    elif user_choice == "quit":
+        print("Goodbye")
+    else:
+        print("Oops - incorrect input")
